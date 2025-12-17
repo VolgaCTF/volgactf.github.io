a132082dcd319137072343324ba4f9e7319c1238
aspyatkin <aspyatkin@users.noreply.github.com>
2025-12-17 20:36:53 +0000

    deployed via Gatsby Publish Action 🎩 for 3951369599769b9034fa80782834495eb4b710c7

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-5e73b215027baf2eceb6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvolgactf_site=self.webpackChunkvolgactf_site||[]).push([[306],{1930:function(e,t,n){var u=n(4836);t.__esModule=!0,t.default=void 0;var a=u(n(7867)),l=u(n(7294)),f=function(e){function t(){return e.apply(this,arguments)||this}return(0,a.default)(t,e),t.prototype.render=function(){return l.default.createElement(l.default.Fragment,null)},t}(l.default.Component),r=f;t.default=r}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-5e73b215027baf2eceb6.js.map
